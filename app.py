d936eca2944b4c317a96cb039bb6738b4c19198a
Luong <ctpluong@gmail.com>
2026-03-27 08:46:37 +0700

    complete app

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from basicsr.archs.srvgg_arch import SRVGGNetCompact
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"  
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # 20MB
 
 UPLOAD_FOLDER = 'uploads'
@@ -15,7 +16,6 @@ OUTPUT_FOLDER = 'outputs'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-# Lazy-load model
 upsampler = None
 def get_upsampler():
     global upsampler
